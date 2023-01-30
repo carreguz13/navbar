94a7838b32f787f518979fd095d186b3ad976407
carreguz13 <carreguz13@gmail.com>
2023-01-29 19:53:12 -0700

    js done using toggle for the navbar in responsive

--- app.js
+++ app.js
@@ -3,3 +3,22 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+var navToggle = document.querySelector(".nav-toggle")
+var links = document.querySelector(".links")
+
+navToggle.addEventListener ("click", function (){
+    //console.log(links.classList);
+//console.log(links.classList.contains("links")); return true
+//console.log(links.classList.contains("random")); return false
+
+//without using toggle
+//if (links.classList.contains("show-links")){
+ //   links.classList.remove("show-links")
+//} else {
+ //   links.classList.add("show-links")
+//}
+
+//using toggle
+links.classList.toggle("show-links")
+})
